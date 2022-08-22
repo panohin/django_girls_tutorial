d2883a585cb7cec0b7968e79bc93d9c109e944a8
Анохин Павел <anokhin.p@nt351.a1tis.ru>
2022-08-22 09:29:30 +0300

    added template and created first view

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
 
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index_url'),
+    path('', views.post_list, name='post_list_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse('Hello world')
+from . import models
+
+
+def post_list(request):
+	context = models.Post.objects.all()
+	return render(request, 'blog/post_list.html', context={})
