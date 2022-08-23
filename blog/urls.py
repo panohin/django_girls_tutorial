aee1c586b4e0005d88f9537141f67b50b1f7e061
Анохин Павел <anokhin.p@nt351.a1tis.ru>
2022-08-23 11:39:54 +0300

    added forms to create and edit posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 # app_name = 'blog'
 urlpatterns = [
+	path('post/<int:pk>/edit/', views.post_edit, name='post_edit_url'),
+	path('post/new/', views.post_new, name='post_new_url'),
 	path('<int:pk>', views.post_detail, name='post_detail_url'),
 	path('', views.post_list, name='post_list_url'),
 ]
